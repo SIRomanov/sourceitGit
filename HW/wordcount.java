c5498f9fa7729bb107048bc3b1b3b040c6b3032f
SIRomanov <56976645+SIRomanov@users.noreply.github.com>
2019-11-21 17:00:03 +0200

    HW4

---
+++ HW/wordcount.java
@@ -0,0 +1,17 @@
+package HW4;
+import java.util.Scanner;
+
+public class wordcount {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("¬ведите текст:");
+		String text = sc.nextLine();
+		sc.close();
+		System.out.println(met(text));
+	}
+	static int met(String text){
+		String[] textArr = text.split("[ ]");
+		return textArr.length;
+		
+	}
+}
